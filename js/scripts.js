f224d195c595e973b4e61eb2b51072217cbfef66
DenysTustanovskiy <tustanovskiyden@gmail.com>
2024-01-31 02:58:04 +0200

    adaptiv

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
-$('.owl-carousel').owlCarousel({ 
+$('.owl-carousel').owlCarousel({
     autoplay:true,
+    autoplayTimeout:2000,
     responsive:{
-                0:{
-                    items:1
-                },
-                1100:{
-                    items:5
-                }
-            }
+        0:{
+            items:1
+        },
+        1100:{
+            items:4
+        }
+    }
 });
